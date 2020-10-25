2fffd9ad63b9f5fb08173096ca9a76fc3a552c77
lorewalker-chen <c52673687@163.com>
2020-10-25 15:52:36 +0800

    Chapter 4

---
+++ Chapter4/dimensions.py
@@ -0,0 +1,6 @@
+#元组用()表示，访问每个元素方法与列表相同，但无法赋值
+dimensions=(200,50)
+print(dimensions[0])
+print(dimensions[1])
+
+#dimensions[0]=100会报错
--- Chapter4/players.py
+++ Chapter4/players.py
@@ -1,2 +1,9 @@
-players=['charles','martina','michael','florence','eli']
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+#切片[起点:终点]
 print(players[0:3])
+#从头
+print(players[:3])
+#到末尾
+print(players[2:])
+#最后几个
+print(players[-3:])
